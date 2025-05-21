300563ac7aa660452da635f7bf46569dd7287fd6
sandro-krd <sandro.krd@gmail.com>
2025-05-21 15:42:46 -0300

    GameMinDTO, GameRepository, GameService, GameController

---
+++ src/main/java/com/example/dslist/repositories/GameRepository.java
@@ -0,0 +1,25 @@
+package com.example.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.dslist.entities.Game;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+	
+	/*
+	 * // Custom query to find games by title
+	 * 
+	 * @Query("SELECT g FROM Game g WHERE g.title LIKE %:title%") List<Game>
+	 * findByTitle(@Param("title") String title);
+	 * 
+	 * // Custom query to find games by genre
+	 * 
+	 * @Query("SELECT g FROM Game g WHERE g.genre = :genre") List<Game>
+	 * findByGenre(@Param("genre") String genre);
+	 * 
+	 * // Custom query to find games by platform
+	 * 
+	 * @Query("SELECT g FROM Game g WHERE g.platforms LIKE %:platform%") List<Game>
+	 * findByPlatform(@Param("platform") String platform);
+	 */
+}
